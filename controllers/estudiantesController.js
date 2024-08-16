6a3ceb521200bd77cca1a9aae2950a39ae9d3407
marc7hedev <driberstagg@gmail.com>
2024-08-16 14:37:50 -0600

    Implementada función para el método actualizar en estudiantesController

--- controllers/estudiantesController.js
+++ controllers/estudiantesController.js
@@ -42,25 +42,43 @@ class EstudiantesController{
         try{
             const { dni, nombre, apellido, email } = req.body;
             db.query(`
-                    INSERT INTO estudiantes
-                        (id, dni, nombre, apellido, email)
-                        VALUES (NULL, ?, ?, ?, ?);`,
-                    [dni, nombre, apellido, email], (err, rows) => {
-                        if(err){
-                            res.status(400).send(err);
-                        }
-                        res.status(201).json({ id: rows.insertId });
-                });
+                INSERT INTO estudiantes
+                (id, dni, nombre, apellido, email)
+                VALUES (NULL, ?, ?, ?, ?);`,
+                [dni, nombre, apellido, email], (err, rows) => {
+                    if(err){
+                        res.status(400).send(err);
+                    }
+                    res.status(201).json({ id: rows.insertId });
+            });
         } catch(err) {
             res.status(500).send(err.message);
         }
     }
 
+
+
     actualizar(req, res){
-        res.json({
-            msg: 'Actualiza estudiante desde clase'
-        });
+        const { id } = req.params;
+        try{
+            const { dni, nombre, apellido, email } = req.body;
+            db.query(`
+                UPDATE apirest.estudiantes
+                SET dni=?, nombre=?, apellido=?, email=?
+                WHERE id=?;`,
+                [dni, nombre, apellido, email, id], (err, rows) => {
+                    if(err){
+                        res.status(400).send(err);
+                    }
+                    if(rows.affectedRows == 1)
+                    res.status(201).json({ respuesta: "Registro actualizado con éxito" });
+            });
+        } catch(err) {
+            res.status(500).send(err.message);
+        }
     }
+
+
 
     borrar(req, res){
         res.json({
